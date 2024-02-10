df2c103ebb73fc0e11a0127cd90cddaaea02cb9f
Atri Bhattacharya <A.Bhattacharya@uliege.be>
2024-02-11 00:57:00 +0530

    test: multi dir LaTeX project.
    Add test for running tartex on a multi dir LaTeX project that checks
    the final dir structure of the tar file contents against that of the
    source dir.

---
+++ tests/test_multidir_latex.py
@@ -0,0 +1,53 @@
+# vim: set ai et ts=4 sw=4 tw=80:
+# SPDX-FileCopyrightText: 2024-present Atri Bhattacharya <A.Bhattacharya@uliege.be>
+#
+# SPDX-License-Identifier: MIT
+
+"""Tests for LaTeX project consisting of multiple dirs"""
+
+import os
+from pathlib import Path
+import tarfile as tar
+
+import pytest
+
+from tartex.tartex import TarTeX
+
+
+@pytest.fixture
+def multidir_target(datadir):
+    return Path(datadir) / "multi"
+
+
+@pytest.fixture
+def multidir_tartex_obj(datadir, multidir_target):
+    return TarTeX(
+        [
+            (Path(datadir) / "main.tex").as_posix(),
+            "-o",
+            multidir_target.as_posix(),
+        ]
+    )
+
+
+class TestMultiDir:
+    """Tests for LaTeX projects with files spread across multiple dirs"""
+
+    def test_tar(self, datadir, multidir_tartex_obj):
+        """Test tar file creation"""
+        t = multidir_tartex_obj
+        src_files = []
+
+        for dname, _, files in os.walk(datadir):
+            for f in files:
+                src_files.append(str(Path(dname).relative_to(datadir) / f))
+
+        src_files.sort()  # Sort for comparison with tar output
+
+        t.tar_files()
+        output = t.tar_file.with_suffix(".tar.gz")
+        assert output.exists()
+
+        with tar.open(output, "r") as rat:
+            # Check if files in tarball have the same dir structure as src_files
+            assert src_files == sorted(rat.getnames())
