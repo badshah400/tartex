badshah400/tartex

commit df2085bf2f865c7db973f40149440345ea7b629b

fix: do not leave git tree in detached state.
When starting work in a git tree where HEAD points to branch FOO tip,
restore back by checking out FOO instead of checking out HEAD by its
commit SHA. We were doing the latter, leaving the tree in a detached
HEAD state. Fixed by this commit.

src/tartex/_git_rev.py
@@ -22,8 +22,16 @@ def git_checkout(git_bin: str, repo: str, rev: str):
     afterwards
     """
 
-    head_full_ref = _get_ref(git_bin, repo, "HEAD")
-    head_short_ref = head_full_ref[:7]
+    head_abbrev_ref = _get_ref(git_bin, repo, "HEAD", abbrev=True)
+    if head_abbrev_ref == "HEAD":
+        # This means we are in a detached working tree to begin with
+        # and will require the full hash to restore tree at the end
+        head_full_ref = _get_ref(git_bin, repo, "HEAD", abbrev=False)
+        head_short_ref = head_full_ref[:7]
+    else:
+        # HEAD points to branch tip
+        head_full_ref = head_short_ref = head_abbrev_ref
+
     try:
         rev_full_ref = _get_ref(git_bin, repo, rev)
         rev_short_ref = rev_full_ref[:7]
@@ -76,11 +84,14 @@ def git_checkout(git_bin: str, repo: str, rev: str):
         yield rev_short_ref
 
 
-def _get_ref(git_bin, repo, rev):
+def _get_ref(git_bin, repo, rev, abbrev=False):
     """Returns the full ref for a given git revision `rev` in a git `repo`"""
 
+    cmd = [git_bin, "-C", repo, "rev-parse"]
+    if abbrev:
+        cmd += ["--abbrev-ref"]
     return subprocess.run(
-        [git_bin, "-C", repo, "rev-parse", rev],
+        [*cmd, rev],
         capture_output=True,
         encoding="utf-8",
         check=True,

commit 673058dc63320bbbc90e656895745db0a037eed9

fix: always use HEAD's full SHA for checkout test.
Doesn't matter if init HEAD is detached or in branch, we must use full
SHA to check if any checkout is needed. This bug was introduced in
commit df2085b.

src/tartex/_git_rev.py
@@ -23,14 +23,14 @@ def git_checkout(git_bin: str, repo: str, rev: str):
     """
 
     head_abbrev_ref = _get_ref(git_bin, repo, "HEAD", abbrev=True)
+    head_full_ref = _get_ref(git_bin, repo, "HEAD", abbrev=False)
     if head_abbrev_ref == "HEAD":
         # This means we are in a detached working tree to begin with
         # and will require the full hash to restore tree at the end
-        head_full_ref = _get_ref(git_bin, repo, "HEAD", abbrev=False)
         head_short_ref = head_full_ref[:7]
     else:
         # HEAD points to branch tip
-        head_full_ref = head_short_ref = head_abbrev_ref
+        head_short_ref = head_abbrev_ref
 
     try:
         rev_full_ref = _get_ref(git_bin, repo, rev)
@@ -63,8 +63,9 @@ def git_checkout(git_bin: str, repo: str, rev: str):
                 log.critical("Git: %s", line)
             raise err
 
+        restore_ref = head_full_ref if head_abbrev_ref == "HEAD" else head_abbrev_ref
         head_proc = subprocess.run(
-            [git_bin, "-C", repo, "checkout", "-f", head_full_ref],
+            [git_bin, "-C", repo, "checkout", "-f", restore_ref],
             capture_output=True,
             encoding="utf-8",
             check=True,