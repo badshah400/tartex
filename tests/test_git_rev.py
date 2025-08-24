c0aa51def65cbf50495331fde0e306913bce1451
Atri Bhattacharya <A.Bhattacharya@uliege.be>
2025-08-24 20:56:56 +0530

    test: more and better tests for `--git-rev`.
    * Use lambdas to set up TarTeX objects.
    * Test that default tarball names have different git suffixes
      corresponding to different git refs.

--- tests/test_git_rev.py
+++ tests/test_git_rev.py
@@ -8,7 +8,7 @@ from pathlib import Path
 from tartex.tartex import TarTeX, TAR_DEFAULT_COMP
 
 @pytest.fixture
-def git_bin() -> Path:
+def git_bin():
     """Returns the git executable available in PATH
     :returns: pathlib.Path
 
@@ -22,34 +22,45 @@ def git_repo_clean(datadir, git_bin, capsys):
 
     """
     git_cmd_base = [git_bin, "-C", datadir]
-    git_cmd = run([*git_cmd_base, "init", "-b", "main"])
+    run([*git_cmd_base, "init", "-b", "main"])
     run([*git_cmd_base, "add", "."])
     run([*git_cmd_base, "commit", "-m", "Initial commit."])
-    run([*git_cmd_base, "ls-tree", "HEAD"], capture_output=True)
     r = run([*git_cmd_base, "rev-parse", "HEAD"],
             capture_output=True,
             encoding="utf-8")
     return datadir, git_cmd_base, r.stdout.splitlines()[0].strip()
 
 
+@pytest.fixture
+def gitrev_tartex(datadir):
+    return lambda rev: TarTeX(
+            [
+                (Path(datadir) / "git_rev").as_posix(),
+                "-g",
+                rev,
+            ]
+        )
+
+
 class TestGitRev:
 
-    """Test for git-rev"""
+    """Test class for git-rev"""
 
-    def test_git_init(self, git_repo_clean, datadir, git_bin):
+    def test_default_tar_filename(self, git_repo_clean, datadir, git_bin, gitrev_tartex):
         git_repo, git, git_ref = git_repo_clean
-        assert git_repo
+        assert git_repo == datadir
         assert git_ref
+        git_short_ref = git_ref[:7]
+        # r1 should tarball working tree as it appeared at first commit
+        r1 = gitrev_tartex(git_ref)
+        assert f".{git_short_ref}" in Path(r1.tar_file_w_ext).suffixes
+
+    def test_tarfile_change_tag(self, git_repo_clean, datadir, git_bin, gitrev_tartex):
+        git_repo, git, git_ref = git_repo_clean
         git_short_ref = git_ref[:7]
         run(["sed", "-i", "s/test document/Test Document/", Path(datadir)/"git_rev.tex"])
         run([*git, "commit", "-a", "-m", "Second commit"])
-        r = TarTeX(
-            [
-                (Path(datadir) / "git_rev").as_posix(),
-                "-v",
-                "-s",
-                "-g",
-                git_ref,
-            ]
-        )
-        assert f".{git_short_ref}" in Path(r.tar_file_w_ext).suffixes
+
+        # r2 should tarball working tree as it appears at current HEAD
+        r2 = gitrev_tartex("")
+        assert f".{git_short_ref}" not in Path(r2.tar_file_w_ext).suffixes
