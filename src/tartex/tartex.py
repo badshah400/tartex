4b584360203826919dbc396c177160401d1dc02c
Atri Bhattacharya <A.Bhattacharya@uliege.be>
2025-10-01 19:26:01 +0530

    fix: do not try to build excl list if not asked.
    When the user does not pass the `-x`/`--excl`, just leave the
    `self.excl_files` set empty and move on.

--- src/tartex/tartex.py
+++ src/tartex/tartex.py
@@ -245,40 +245,43 @@ class TarTeX:
 
         self.req_supfiles: dict[Path, Any] = {}
         self.add_files = self.args.add.split(",") if self.args.add else []
-        excludes = self.args.excl.split(",") if self.args.excl else []
-        excl_lists = (self.main_file.parent.glob(f"**/{L}") for L in excludes)
-
-        self.excl_files = set(
-            [
-                f.relative_to(self.main_file.parent)
-                for L in excl_lists
-                for f in L
-            ]
-        )
-        # Do not exclude main tex file even if matched by `--excl` glob
-        self.excl_files.discard(
-            self.main_file.with_suffix(".tex").relative_to(
-                self.main_file.parent
+        self.excl_files: set[Path] = set()
+        if self.args.excl:
+            excludes = self.args.excl.split(",") if self.args.excl else []
+            excl_lists = (
+                self.main_file.parent.glob(f"**/{L}") for L in excludes
             )
-        )
 
-        # If .bbl/.ind is missing in source dir, then it is not in
-        # self.excl_files (result of globbing files in srcdir) even if the user
-        # passes a matching wildcard to exclude it with "-x".
-        # Extend self.excl_files specifically in these cases
-        for glb in excludes:
             self.excl_files.update(
                 [
-                    f
-                    for f in [
-                        self.main_file.with_suffix(f".{g}")
-                        for g in _tartex_tex_utils.SUPP_REQ
-                    ]
-                    if fnmatch.fnmatch(f.name, glb)
+                    f.relative_to(self.main_file.parent)
+                    for L in excl_lists
+                    for f in L
                 ]
             )
+            # Do not exclude main tex file even if matched by `--excl` glob
+            self.excl_files.discard(
+                self.main_file.with_suffix(".tex").relative_to(
+                    self.main_file.parent
+                )
+            )
 
-        if self.excl_files:
+            # If .bbl/.ind is missing in source dir, then it is not in
+            # self.excl_files (result of globbing files in srcdir) even if the
+            # user passes a matching wildcard to exclude it with "-x".  Extend
+            # self.excl_files specifically in these cases
+            for glb in excludes:
+                self.excl_files.update(
+                    [
+                        f
+                        for f in [
+                            self.main_file.with_suffix(f".{g}")
+                            for g in _tartex_tex_utils.SUPP_REQ
+                        ]
+                        if fnmatch.fnmatch(f.name, glb)
+                    ]
+                )
+
             log.debug(" ".join(excl_lists))
             log.info(
                 "List of excluded files: %s",
