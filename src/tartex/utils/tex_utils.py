be2f0ad5ccc1729ef5ec0d2fa7039eddf908f7e6
Atri Bhattacharya <A.Bhattacharya@uliege.be>
2025-10-04 23:22:25 +0530

    chore: reuse linebreak var, plus formatting fixes.

--- src/tartex/utils/tex_utils.py
+++ src/tartex/utils/tex_utils.py
@@ -89,28 +89,36 @@ def latexmk_summary(err_msg: str) -> tuple[set[str], set[str]]:
     """
     LBR = r"(?:\n|\r\n)"
     RE_ERRORS: dict[str, Union[str, None]] = {
-        # "search pattern"                             : "replacement" or None
-        r"^! LaTeX Error: (Environment .* undefined).$": r"\1",
+        # "search pattern"
+        # : "replacement" or None
+        r"^! LaTeX Error: (Environment .* undefined).$"
+        : r"\1",
         r"^! LaTeX Error: (Missing \begin\{document\}).$"
-                                                       : r"\1",
-        rf"^(.*){LBR}! (Emergency stop).{LBR}(.*)$"    : r"\2: \1",
-        r"^! LaTeX Error: (File `.*' not found.)$"     : r"\1",
-        r"^! (Package.* Error: .* not found):"         : r"\1",
+        : r"\1",
+        rf"^(.*){LBR}! (Emergency stop).{LBR}(.*)$"
+        : r"\2: \1",
+        r"^! LaTeX Error: (File `.*' not found.)$"
+        : r"\1",
+        r"^! (Package.* Error: .* not found):"
+        : r"\1",
         rf"^! (Undefined control sequence).{LBR}(l\.\d+)\s*(.*)$"
-                                                       : r"\2 [\1]: \3",
-        rf"^! (Too many \{{?\}}?'s.){LBR}(l\.\d+)(.*)$": r"\2 [\1]: \3",
+        : r"\2 [\1]: \3",
+        rf"^! (Too many \{{?\}}?'s.){LBR}(l\.\d+)(.*)$"
+        : r"\2 [\1]: \3",
         r"^! (Missing \}?\{?\$? inserted)."
-        rf"{LBR}(?:.*){LBR}(?:.*){LBR}(l\.\d+)\s(.*)$" : r"\2 [\1]: \3",
-        r"^Runaway argument\?"                         : None,
+        rf"{LBR}(?:.*){LBR}(?:.*){LBR}(l\.\d+)\s(.*)$"
+        : r"\2 [\1]: \3",
+        r"^Runaway argument\?"
+        : None,
         rf"^! (Misplaced alignment .*) \&\.{LBR}(l.\d+)\s*(.*\&)$"
-                                                       : r"\2 [\1]: \3",
+        : r"\2 [\1]: \3",
     }
 
     err_lines = _get_filtered_lines(RE_ERRORS, err_msg)
 
     RE_WARNS: dict[str, Union[str, None]] = {
         r"^LaTeX Warning: (.*)\.$" : r"\1",
-        r"^Package (.*) Warning: (.*(?:\n|\r\n)?.*\.)$" : r"\1 warning: \2",
+        rf"^Package (.*) Warning: (.*{LBR}?.*\.)$" : r"\1 warning: \2",
     }
     warn_lines: set[str] = _get_filtered_lines(
         RE_WARNS,
